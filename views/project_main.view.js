19e9cc3209096a98659d068c450cc2922075da34
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-20 11:35:39 -0700

    1135

--- views/project_main.view.js
+++ views/project_main.view.js
@@ -9,7 +9,6 @@ sap.ui.jsview("project.views.project_main", {
 
     this.setHeight("100%"); 
 
-
     var item = new sap.m.ListItemBase({
       text: "Test"
     });
@@ -50,67 +49,7 @@ sap.ui.jsview("project.views.project_main", {
     splitapp.addDetailPage(detailpage); 
 
 
-
-
-
-
-
-
-
-
-
-    // HEADER BAR 
-
-    // var homeBtn = new sap.m.Button(this.createId("project-homebutton"), {
-    //   icon: "sap-icon://home",
-    //   tooltip: "Home",
-    //   press: function() {
-    //     alert("Home sweet home");
-    //   }
-    // });
-    // oController.homeBtn = homeBtn; 
-
-    // var headerLabel = new sap.m.Label(this.createId("project-label"), {
-    //   text: "project"
-    // }); 
-
-    // var filterBtn = new sap.m.Button(this.createId("project-filterBtnnbutton"),{
-    //   icon: "sap-icon://add-filter",
-    //   tooltip: "Filter",
-    //   press: function(e) {
-    //     oController.openFilterPanel();
-    //   }
-    // });
-    // oController.filterBtn = filterBtn; 
-
-
-    // var appHeader = new sap.m.Bar(this.createId("app-headerBar"), {      
-    //   contentLeft: [homeBtn],
-    //   contentMiddle: [headerLabel],
-    //   contentRight: [filterBtn]
-    // }).addStyleClass("as-app-header");
-
-    // var paginationHeader = new sap.m.Bar(this.createId("project-headerBar"), {});
-    // oController.paginationHeader = paginationHeader; 
-
-
-    // // CONTENT TABLE 
-    // var contentTable = new sap.m.Table({
-    //   // mode: sap.m.ListMode.MultiSelect,
-    //   // growing: false,
-    //   columns : [
-    //     new sap.m.Column({
-    //       hAlign : "Center",
-    //       header : new sap.m.Label({
-    //         text: "Content: "
-    //       })
-    //     })
-    //   ]
-    // });
-    // oController.contentTable = contentTable;     
-
     var oPage = new sap.m.Page({
-      // customHeader: appHeader, 
       content: [splitapp]
     })
 
