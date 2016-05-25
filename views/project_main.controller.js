e2ba8a425151f54353444c535fae2449b1baf6fb
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-24 17:11:08 -0700

    cleaned code

--- views/project_main.controller.js
+++ views/project_main.controller.js
@@ -1,40 +1,8 @@
-var that; 
-var obj = {};
-var data = []; 
-var currentpage = 1; 
-for (var i = 0; i < 100; i++) {
-  data.push(i.toString()); 
-};
-// ajax
-
-obj = {
-  pagesint: 1,
-  all: data
-}
-var datacursor = 0; 
-var page = 1; 
-while (datacursor < data.length){
-  obj[page.toString()] = [];
-  // 100 instead of 10 for records
-  for (var i = 0; i < 10; i++) {
-    if (datacursor < data.length) {   
-      obj[page.toString()].push(data[datacursor]);
-      datacursor++; 
-    };
-  };
-  page++; 
-}
-obj.pagesint = page - 1; 
-
 sap.ui.controller("project.views.project_main", {
   
   //bus : sap.ui.getCore().getEventBus(),
 
-
-
-
   onInit: function() {
-    console.log("on init main controller");
     this.app = sap.ui.getCore().byId("project-app");
     this.list.attachItemPress(this.listSelection, this);
     this.fetchdata(); 
@@ -66,26 +34,8 @@ sap.ui.controller("project.views.project_main", {
         that.dataarray = data; 
         console.log(that.dataarray);
         
-        
-        // for (var i = 0; i < data.length; i++) {
-        //   var itemContainer = new sap.m.VBox();
-        //   var name = new sap.m.Label({
-        //     text: data[i].name
-        //   })
-        //   var email = new sap.m.Label({
-        //     text: data[i].email
-        //   })
-
-        //   itemContainer.addItem(name);
-        //   itemContainer.addItem(email);
-
-        //   var $item = new sap.m.CustomListItem({
-        //     content: itemContainer
-        //   })
-        //   that.list.addItem($item); 
-        // };
+  
         that.olistModel.setData(that.dataarray);
-        // that.otableModel.setData(that.dataarray);
         that.list.setModel(that.olistModel);
 
       }, 
@@ -109,33 +59,6 @@ sap.ui.controller("project.views.project_main", {
 
       this.contentTable.addColumn($item); 
     };
-  },
-
-  paginationrender: function(){
-    for (var i = 1; i <= obj.pagesint; i++) {
-      var $page = new sap.m.Button({
-        text: i.toString(),
-        press: this.pageclick
-      });
-    
-      this.paginationHeader.addContentLeft($page);
-    };
-    this.display(); 
-  },
-
-  pageclick: function() {
-    var btnnum = this.getText(); 
-    // if (btnnum === "1") {
-    //   console.log("First page");
-    // };
-    // if (btnnum === obj.pagesint.toString()) {
-    //   console.log("Last page");
-    // };
-    currentpage = btnnum; 
-    console.log(currentpage); 
-    // this.oParent.display(); 
-    // that.display(); 
-    // console.log(this.oParent);
   },
 
   selectFirstItem: function(event) {
